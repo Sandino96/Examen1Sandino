fe24fdbc612dac92dabd1ceb72104f00bdc255be
Rodrigo José Sandino Salinas <sandinorodrigo@unitec.edu>
2016-05-20 18:00:14 -0600

    Examen mejorado

--- Examen1.cpp
+++ Examen1.cpp
@@ -41,8 +41,11 @@ void menu(){
 			cout << "Ingrese su morse: ";
 			cin.ignore();
 			cin.getline(morse,144);
-			cout << "Palabra creada: " << ejercicio3(morse) << endl;
-			delete[] ejercicio3(morse);
+			cout << "Palabra creada: ";
+			for (int i = 0; ejercicio3(morse)[i]; i++){
+				cout << ejercicio3(morse)[i];
+			}
+			cout << endl;
 			delete[] morse;
 		}
 	}
@@ -56,7 +59,7 @@ bool ejercicio1(char* frase){
 		for(int j = 0; letras[j]; j++){
 			if (frase[i] == letras[j]){
 				letrasIngresadas[j] = letras[j];
-				cout << frase[i] << endl;
+				cout << letrasIngresadas[j] << endl;
 			}
 		}
 	}
@@ -70,6 +73,8 @@ bool ejercicio1(char* frase){
 void ejercicio2(){
 	int decision = 1;
 	char* totalDeFrases = new char[500];
+	char letras[] = "abcdefghijklmnopqrstuvwxyz";
+	char* letrasUsadas = new char[144];
 	while (decision == 1){
 		int contador = 0, contadorPalabras = 0;
 		char* frase = new char[144];
@@ -85,12 +90,22 @@ void ejercicio2(){
 				contador = 0;
 			}
 		}
+		for (int i = 0; frase[i]; i++){
+			letrasUsadas[0] = frase[0];
+			for (int j = 1; letrasUsadas[j]; j++){
+				if(frase[i] != letrasUsadas[j]){
+					letrasUsadas[j] = frase[i];
+				}
+			}
+		}
 		contadorPalabras++;
 		cout << "Palabra " << contadorPalabras << " = " << contador << endl;
 		cout << "Hay " << contadorPalabras << " palabras" << endl;
+		cout << "Letras usadas: " << letrasUsadas << endl;
 		strcat(totalDeFrases, frase);
 		cout << "Desea agregar otra frase: [1=Si/2=No]: ";
 		cin >> decision;
+		delete[] letrasUsadas;
 		delete[] frase;
 	}
 	int imprimirFrases;
@@ -107,100 +122,97 @@ void ejercicio2(){
 
 char* ejercicio3(char* morse){
 	char* nuevaPalabra = new char[144];
-	int size = 0;
-	char temporal[4];
+	int size = 0, j = 0;
+	char temporal[5];
 	for (int i = 0; morse[i]; i++){
 		if(morse[i] != '&'){
-			for (int j = 0; temporal[j]; j++){
-				temporal[j] = morse[i];
-			}
-			if(temporal[0] == '.' && temporal[1] == '-'){
-				nuevaPalabra[size] = 'a';
-				size++;
-			} else if (temporal[0] == '-' && temporal[1] == '.' && temporal[2] == '.' && temporal[3] == '.'){
-				nuevaPalabra[size] = 'b';
-				size++;
-			} else if (temporal[0] == '-' && temporal[1] == '.' && temporal[2] == '-' && temporal[3] == '.'){
-				nuevaPalabra[size] = 'c';
-				size++;
-			} else if (temporal[0] == '-' && temporal[1] == '.' && temporal[2] == '.'){
-				nuevaPalabra[size] = 'd';
-				size++;
-			} else if (temporal[0] == '.'){
-				nuevaPalabra[size] = 'e';
-				size++;
-			} else if (temporal[0] == '.' && temporal[1] == '.' && temporal[2] == '-' && temporal[3] == '.'){
-				nuevaPalabra[size] = 'f';
-				size++;
-			} else if (temporal[0] == '-' && temporal[1] == '-' && temporal[2] == '.'){
-				nuevaPalabra[size] = 'g';
-				size++;
-			} else if (temporal[0] == '.' && temporal[1] == '.' && temporal[2] == '.' && temporal[3] == '.'){
-				nuevaPalabra[size] = 'h';
-				size++;
-			} else if (temporal[0] == '.' && temporal[1] == '.'){
-				nuevaPalabra[size] = 'i';
-				size++;
-			} else if (temporal[0] == '.' && temporal[1] == '-' && temporal[2] == '-' && temporal[3] == '-'){
-				nuevaPalabra[size] = 'j';
-				size++;
-			} else if (temporal[0] == '-' && temporal[1] == '.' && temporal[2] == '-'){
-				nuevaPalabra[size] = 'k';
-				size++;
-			} else if (temporal[0] == '.' && temporal[1] == '-' && temporal[2] == '.' && temporal[3] == '.'){
-				nuevaPalabra[size] = 'l';
-				size++;
-			} else if (temporal[0] == '-' && temporal[1] == '-'){
-				nuevaPalabra[size] = 'm';
-				size++;
-			} else if (temporal[0] == '-' && temporal[1] == '.'){
-				nuevaPalabra[size] = 'n';
-				size++;
-			} else if (temporal[0] == '-' && temporal[1] == '-' && temporal[2] == '-'){
-				nuevaPalabra[size] = 'o';
-				size++;
-			} else if (temporal[0] == '.' && temporal[1] == '-' && temporal[2] == '-' && temporal[3] == '.'){
-				nuevaPalabra[size] = 'p';
-				size++;
-			} else if (temporal[0] == '-' && temporal[1] == '-' && temporal[2] == '.' && temporal[3] == '-'){
-				nuevaPalabra[size] = 'q';
-				size++;
-			} else if (temporal[0] == '.' && temporal[1] == '-' && temporal[2] == '.'){
-				nuevaPalabra[size] = 'r';
-				size++;
-			} else if (temporal[0] == '.' && temporal[1] == '.' && temporal[2] == '.'){
-				nuevaPalabra[size] = 's';
-				size++;
-			} else if (temporal[0] == '-'){
-				nuevaPalabra[size] = 't';
-				size++;
-			} else if (temporal[0] == '.' && temporal[1] == '.' && temporal[2] == '-'){
-				nuevaPalabra[size] = 'u';
-				size++;
-			} else if (temporal[0] == '.' && temporal[1] == '.' && temporal[2] == '.' && temporal[3] == '-'){
-				nuevaPalabra[size] = 'v';
-				size++;
-			} else if (temporal[0] == '.' && temporal[1] == '-' && temporal[2] == '-'){
-				nuevaPalabra[size] = 'w';
-				size++;
-			} else if (temporal[0] == '-' && temporal[1] == '.' && temporal[2] == '.' && temporal[3] == '-'){
-				nuevaPalabra[size] = 'x';
-				size++;
-			} else if (temporal[0] == '-' && temporal[1] == '.' && temporal[2] == '-' && temporal[3] == '-'){
-				nuevaPalabra[size] = 'y';
-				size++;
-			} else if (temporal[0] == '-' && temporal[1] == '-' && temporal[2] == '.' && temporal[3] == '.'){
-				nuevaPalabra[size] = 'z';
-				size++;
-			} else {
-				return nuevaPalabra;
-			}
-		} else {
-			for (int i = 0; i < 4; i++){
-				temporal[i] = '\0';
-			}
-			size = 0;
+			temporal[j] = morse[i];
+			j++;
 		}
 	}
+	for (int i = 0; temporal[i]; i++){
+		if(temporal[0] == '.' && temporal[1] == '-'){
+			nuevaPalabra[size] = 'a';
+			size++;
+		} else if (temporal[0] == '-' && temporal[1] == '.' && temporal[2] == '.' && temporal[3] == '.'){
+			nuevaPalabra[size] = 'b';
+			size++;
+		} else if (temporal[0] == '-' && temporal[1] == '.' && temporal[2] == '-' && temporal[3] == '.'){
+			nuevaPalabra[size] = 'c';
+			size++;
+		} else if (temporal[0] == '-' && temporal[1] == '.' && temporal[2] == '.'){
+			nuevaPalabra[size] = 'd';
+			size++;
+		} else if (temporal[0] == '.'){
+			nuevaPalabra[size] = 'e';
+			size++;
+		} else if (temporal[0] == '.' && temporal[1] == '.' && temporal[2] == '-' && temporal[3] == '.'){
+			nuevaPalabra[size] = 'f';
+			size++;
+		} else if (temporal[0] == '-' && temporal[1] == '-' && temporal[2] == '.'){
+			nuevaPalabra[size] = 'g';
+			size++;
+		} else if (temporal[0] == '.' && temporal[1] == '.' && temporal[2] == '.' && temporal[3] == '.'){
+			nuevaPalabra[size] = 'h';
+			size++;
+		} else if (temporal[0] == '.' && temporal[1] == '.'){
+			nuevaPalabra[size] = 'i';
+			size++;
+		} else if (temporal[0] == '.' && temporal[1] == '-' && temporal[2] == '-' && temporal[3] == '-'){
+			nuevaPalabra[size] = 'j';
+			size++;
+		} else if (temporal[0] == '-' && temporal[1] == '.' && temporal[2] == '-'){
+			nuevaPalabra[size] = 'k';
+			size++;
+		} else if (temporal[0] == '.' && temporal[1] == '-' && temporal[2] == '.' && temporal[3] == '.'){
+			nuevaPalabra[size] = 'l';
+			size++;
+		} else if (temporal[0] == '-' && temporal[1] == '-'){
+			nuevaPalabra[size] = 'm';
+			size++;
+		} else if (temporal[0] == '-' && temporal[1] == '.'){
+			nuevaPalabra[size] = 'n';
+			size++;
+		} else if (temporal[0] == '-' && temporal[1] == '-' && temporal[2] == '-'){
+			nuevaPalabra[size] = 'o';
+			size++;
+		} else if (temporal[0] == '.' && temporal[1] == '-' && temporal[2] == '-' && temporal[3] == '.'){
+			nuevaPalabra[size] = 'p';
+			size++;
+		} else if (temporal[0] == '-' && temporal[1] == '-' && temporal[2] == '.' && temporal[3] == '-'){
+			nuevaPalabra[size] = 'q';
+			size++;
+		} else if (temporal[0] == '.' && temporal[1] == '-' && temporal[2] == '.'){
+			nuevaPalabra[size] = 'r';
+			size++;
+		} else if (temporal[0] == '.' && temporal[1] == '.' && temporal[2] == '.'){
+			nuevaPalabra[size] = 's';
+			size++;
+		} else if (temporal[0] == '-'){
+			nuevaPalabra[size] = 't';
+			size++;
+		} else if (temporal[0] == '.' && temporal[1] == '.' && temporal[2] == '-'){
+			nuevaPalabra[size] = 'u';
+			size++;
+		} else if (temporal[0] == '.' && temporal[1] == '.' && temporal[2] == '.' && temporal[3] == '-'){
+			nuevaPalabra[size] = 'v';
+			size++;
+		} else if (temporal[0] == '.' && temporal[1] == '-' && temporal[2] == '-'){
+			nuevaPalabra[size] = 'w';
+			size++;
+		} else if (temporal[0] == '-' && temporal[1] == '.' && temporal[2] == '.' && temporal[3] == '-'){
+			nuevaPalabra[size] = 'x';
+			size++;
+		} else if (temporal[0] == '-' && temporal[1] == '.' && temporal[2] == '-' && temporal[3] == '-'){
+			nuevaPalabra[size] = 'y';
+			size++;
+		} else if (temporal[0] == '-' && temporal[1] == '-' && temporal[2] == '.' && temporal[3] == '.'){
+			nuevaPalabra[size] = 'z';
+			size++;
+		} else {
+			return nuevaPalabra;
+		}
+		break;
+	}
 	return nuevaPalabra;
-}
+} 
